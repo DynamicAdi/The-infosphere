9b1b2b50c1bd9da26244537ac5826dabd61263dc
Ɗყηᴀʍɨᴄ ◠‿◕ <adarshpanditdev@gmail.com>
2024-09-04 16:50:59 +0530

    Fixes

--- frontend/components/ui/home/page.tsx
+++ frontend/components/ui/home/page.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import styles from './styles.module.scss';
-import Navbar from '@/components/core/navbar/page';
 import FadeCarousel from '../MenuCard/page';
 import { getPost } from '@/lib/calls';
 import { dataProps } from '@/lib/types';
-import UpperSection from '@/components/core/upperSection/page';
 import Header from '@/components/core/header/page';
 
 async function HomeSection() {
